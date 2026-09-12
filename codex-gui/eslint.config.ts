80c8f58b87ae9e3967426e1c619c45eaf704bbac
Jiang Sheng <jiangshengdev@outlook.com>
2026-09-12 19:58:10 +0800

    chore(gui): initialize Storybook with example stories

--- codex-gui/eslint.config.ts
+++ codex-gui/eslint.config.ts
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import path from "node:path";
 import js from "@eslint/js";
 import globals from "globals";
